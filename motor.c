0aec72500b0dd4ecb35a76a611dac0b02f9a6751
刘子宸 <scratcher@foxmail.com>
2022-07-25 10:29:15 +0800

    修复愚蠢错误

--- motor.c
+++ motor.c
@@ -6,6 +6,7 @@ void motor_init(struct motor *m) {
     uint slice_num_a = pwm_gpio_to_slice_num(m->pin_a);
     uint slice_num_b = pwm_gpio_to_slice_num(m->pin_b);
     pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 4.0);
     pwm_init(slice_num_a, &config, true);
     pwm_init(slice_num_b, &config, true);
     pwm_set_gpio_level(m->pin_a, 0);
@@ -17,7 +18,6 @@ inline void motor_set_pwm_duty(struct motor *m, int speed) {
         pwm_set_gpio_level(m->pin_a, 0);
         pwm_set_gpio_level(m->pin_b, 0);
     } else if (speed > 0) {
-        speed = -speed;
         speed &= 0xFFFF;
         pwm_set_gpio_level(m->pin_b, 0);
         pwm_set_gpio_level(m->pin_a, speed);
